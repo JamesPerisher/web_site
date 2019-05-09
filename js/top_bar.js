23e77381a7daaedd2e222f8842e29875f60d3d2d
PaulN07 <JamesPerisher@users.noreply.github.com>
2019-05-10 05:23:45 +1000

    partialy added mobile support for top navagator

--- js/top_bar.js
+++ js/top_bar.js
@@ -24,13 +24,19 @@ var isMobile = {
 function topbar(current) { // default navagarion bar HTML
   var a = "<div class=\"id564\">\
       \<div class=\"mobile\"><div class=\"topnav\">\
+      \<div id=\"myLinks\">\
       \<a class=\"cat home\" href=\"home.html\">Home</a>\
       \<a class=\"cat about\" href=\"about.html\">About</a>\
       \<a class=\"cat discord_bot\" href=\"discord_bot.html\">Discord Bot</a>\
       \<a class=\"cat res_pack\" href=\"res_pack.html\">Resource pack</a>\
+      \</div>\
+      \<a href=\"javascript:void(0);\" class=\"icon\" onclick=\"mobile_button()\">\
+        \<i class=\"fa fa - bars \"></i>hello\
+      \</a>\
       \ \
       \<a class=\"right\" href=\"https://discord.gg/2wj94TP\" target=\"_blank\">Join my discord</a>\
     \</div></div> </div>";
+
 
 
 
@@ -52,8 +58,20 @@ function topbar(current) { // default navagarion bar HTML
     var d1 = document.getElementsByTagName("header")[0];
     d1.insertAdjacentHTML('beforeEnd', doc.firstChild.querySelector('body > div.id564').innerHTML);
   } else { // display pc version
+    doc.querySelector("#myLinks").setAttribute("id", "none");
     var d1 = document.getElementsByTagName("header")[0];
     d1.insertAdjacentHTML('beforeEnd', doc.firstChild.querySelector('body > div.id564').innerHTML);
   };
 };
+
+function mobile_button() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+
 topbar(document.URL.split("/").pop().split(".")[0]); // may break with webpages with no filename extensions
